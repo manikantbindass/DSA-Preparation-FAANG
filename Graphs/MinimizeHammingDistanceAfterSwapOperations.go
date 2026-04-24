aed59e86768d2ee6d047cacafbd8bc3beaa5e4c2
Manikant Bindass <manikantbindass@rediffmail.com>
2026-04-24 19:09:10 +0530

    chore: add Go and Python stubs for existing Java problems

---
+++ Graphs/MinimizeHammingDistanceAfterSwapOperations.go
@@ -0,0 +1,6 @@
+package graphs
+
+func minimumHammingDistance(source []int, target []int, allowedSwaps [][]int) int {
+	// TODO: implement MinimizeHammingDistanceAfterSwapOperations in Go
+	return 0
+}
