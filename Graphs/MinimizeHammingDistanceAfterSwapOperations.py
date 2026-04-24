d2fd944cc3ceb58da0edf14810ba6120a4d3f3ad
Manikant Bindass <manikantbindass@rediffmail.com>
2026-04-24 19:09:44 +0530

    chore: add Go and Python stubs for existing Java problems

---
+++ Graphs/MinimizeHammingDistanceAfterSwapOperations.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def minimumHammingDistance(source: List[int], target: List[int], allowedSwaps: List[List[int]]) -> int:
+    # TODO: implement MinimizeHammingDistanceAfterSwapOperations in Python
+    pass
