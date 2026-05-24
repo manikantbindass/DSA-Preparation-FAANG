565947d48b89e0b9e0d674eb0757e5be61c1a8cc
Manikant <manikantbindass@rediffmail.com>
2026-05-24 12:49:17 +0530

    Implement Unique Binary Search Trees solution

---
+++ DP/UniqueBinarySearchTrees_96.java
@@ -0,0 +1,16 @@
+// 96. Unique Binary Search Trees
+// https://leetcode.com/problems/unique-binary-search-trees/
+// Difficulty: Medium
+
+class Solution {
+    public int numTrees(int n) {
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                f[i] += f[j] * f[i - j - 1];
+            }
+        }
+        return f[n];
+    }
+}
