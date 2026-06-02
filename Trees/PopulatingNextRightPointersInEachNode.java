b3e8e0db1ee5c95fc8aa79b00ce98ae1c9d2f86c
Manikant <manikantbindass@rediffmail.com>
2026-06-02 14:08:08 +0530

    Create PopulatingNextRightPointersInEachNode.java

---
+++ Trees/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,89 @@
+/*
+ * LeetCode Problem 116: Populating Next Right Pointers in Each Node
+ * Problem Number: 116
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+ * 
+ * You are given a perfect binary tree where all leaves are at the same level,
+ * and every parent has two children. Populate each next pointer to point to its
+ * next right node. If there is no next right node, the next pointer should be set to NULL.
+ * 
+ * Initially, all next pointers are set to NULL.
+ * 
+ * Example 1:
+ * Input: root = [1,2,3,4,5,6,7]
+ * Output: [1,#,2,3,#,4,5,6,7,#]
+ * 
+ * Example 2:
+ * Input: root = []
+ * Output: []
+ * 
+ * Constraints:
+ * - The number of nodes in the tree is in the range [0, 2^12 - 1].
+ * - -1000 <= Node.val <= 1000
+ * 
+ * Topics: Linked List, Tree, Depth-First Search, Breadth-First Search, Binary Tree
+ * Time Complexity: O(n) - visit each node once
+ * Space Complexity: O(w) - where w is the maximum width of the tree (queue size)
+ */
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        
+        Deque<Node> queue = new ArrayDeque<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            Node prev = null;
+            
+            for (int i = 0; i < levelSize; i++) {
+                Node curr = queue.poll();
+                
+                // Connect previous node to current node
+                if (prev != null) {
+                    prev.next = curr;
+                }
+                prev = curr;
+                
+                // Add children to queue
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+        }
+        
+        return root;
+    }
+}
