d37b390353919e0ff3526742e12aa6c9014419f3
Manikant <manikantbindass@rediffmail.com>
2026-06-07 08:25:47 +0530

    Add solution for creating binary tree from descriptions
    Implement a solution to create a binary tree from given descriptions, handling parent-child relationships and identifying the root node.

---
+++ Trees/CreateBinaryTreeFromDescriptions.java
@@ -0,0 +1,89 @@
+/*
+ * LeetCode Problem 2196: Create Binary Tree From Descriptions
+ * Problem Number: 2196
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/create-binary-tree-from-descriptions/
+ * 
+ * You are given a 2D integer array descriptions where descriptions[i] = [parent_i, child_i, isLeft_i]
+ * indicates that parent_i is the parent of child_i in a binary tree.
+ * 
+ * Return the root of the binary tree.
+ * 
+ * Example 1:
+ * Input: descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+ * Output: [50,20,80,15,17,19]
+ * 
+ * Example 2:
+ * Input: descriptions = [[1,2,1],[2,3,0],[3,4,1]]
+ * Output: [1,2,null,null,3,4]
+ * 
+ * Constraints:
+ * - 1 <= descriptions.length <= 10^4
+ * - descriptions[i].length == 3
+ * - 1 <= parent_i, child_i <= 10^5
+ * - 0 <= isLeft_i <= 1
+ * 
+ * Topics: Array, Hash Table, Tree, Binary Tree
+ * Time Complexity: O(n) - single pass through descriptions
+ * Space Complexity: O(n) - for storing nodes and children set
+ */
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        Map<Integer, TreeNode> nodeMap = new HashMap<>();
+        Set<Integer> children = new HashSet<>();
+        
+        for (int[] desc : descriptions) {
+            int parentVal = desc[0];
+            int childVal = desc[1];
+            int isLeft = desc[2];
+            
+            // Get or create parent node
+            nodeMap.putIfAbsent(parentVal, new TreeNode(parentVal));
+            // Get or create child node
+            nodeMap.putIfAbsent(childVal, new TreeNode(childVal));
+            
+            TreeNode parent = nodeMap.get(parentVal);
+            TreeNode child = nodeMap.get(childVal);
+            
+            // Set left or right child
+            if (isLeft == 1) {
+                parent.left = child;
+            } else {
+                parent.right = child;
+            }
+            
+            // Mark child as having a parent
+            children.add(childVal);
+        }
+        
+        // Find the root (node that is never a child)
+        for (int val : nodeMap.keySet()) {
+            if (!children.contains(val)) {
+                return nodeMap.get(val);
+            }
+        }
+        
+        return null;
+    }
+}
