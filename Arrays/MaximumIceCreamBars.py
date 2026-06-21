400dfd93efe0f5120d3668058ba18b935fe3eef1
Manikant <manikantbindass@rediffmail.com>
2026-06-21 08:24:21 +0530

    Add solution for Maximum Ice Cream Bars problem
    Implement solution for LeetCode Problem 1833, calculating the maximum number of ice cream bars that can be bought with given coins.

---
+++ Arrays/MaximumIceCreamBars.py
@@ -0,0 +1,48 @@
+"""
+LeetCode Problem 1833: Maximum Ice Cream Bars
+Problem Number: 1833
+Difficulty: Medium
+Link: https://leetcode.com/problems/maximum-ice-cream-bars/
+
+You are given an array costs where costs[i] is the price of the i-th ice cream bar.
+You have an integer coins representing the total amount of coins you have.
+Return the maximum number of ice cream bars you can buy.
+
+Example 1:
+Input: costs = [1,3,2,4,1], coins = 7
+Output: 4
+Explanation: You can buy bars with costs [1,1,2,3] for a total of 7 coins.
+
+Example 2:
+Input: costs = [10,6,8,7,7,8], coins = 5
+Output: 0
+
+Example 3:
+Input: costs = [1,6,3,1,2,5], coins = 20
+Output: 6
+
+Constraints:
+- 1 <= costs.length <= 10^5
+- 1 <= costs[i] <= 10^5
+- 1 <= coins <= 10^8
+
+Topics: Array, Greedy, Sorting
+Time Complexity: O(n log n) - due to sorting
+Space Complexity: O(1) - excluding the space for sorting
+"""
+
+from typing import List
+
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        # Sort costs in ascending order to buy cheapest first
+        costs.sort()
+        
+        count = 0
+        for cost in costs:
+            if coins < cost:
+                break
+            coins -= cost
+            count += 1
+        
+        return count
