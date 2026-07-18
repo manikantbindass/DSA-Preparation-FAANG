2cbf06dc37d673b62c796681709dc4eda20925c5
Manikant <manikantbindas@gmail.com>
2026-07-18 21:32:34 +0530

    feat: add Lowest Common Ancestor of a Binary Search Tree (#235)

---
+++ Trees/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,107 @@
+/*
+ * LeetCode Problem 235: Lowest Common Ancestor of a Binary Search Tree
+ * Problem Number: 235
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ *
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) node of
+ * two given nodes in the BST.
+ *
+ * According to the definition of LCA on Wikipedia: &ldquo;The lowest common
+ * ancestor is defined between two nodes p and q as the lowest node in T that has
+ * both p and q as descendants (where we allow a node to be a descendant of
+ * itself).&rdquo;
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * Output: 6
+ * Explanation: The LCA of nodes 2 and 8 is 6.
+ *
+ * Example 2:
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * Output: 2
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of
+ * itself according to the LCA definition.
+ *
+ * Example 3:
+ *
+ * Input: root = [2,1], p = 2, q = 1
+ * Output: 2
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 	The number of nodes in the tree is in the range [2, 105].
+ * 	-109 <= Node.val <= 109
+ * 	All Node.val are unique.
+ * 	p != q
+ * 	p and q will exist in the BST.
+ *
+ * Example 1:
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * Output: 6
+ * Explanation: The LCA of nodes 2 and 8 is 6.
+ *
+ * Example 2:
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * Output: 2
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+ *
+ * Example 3:
+ * Input: root = [2,1], p = 2, q = 1
+ * Output: 2
+ *
+ * Constraints:
+ * - The number of nodes in the tree is in the range [2, 105].
+ * - -109 <= Node.val <= 109
+ * - All Node.val are
+ *
+ * Topics: Tree, Depth-First Search, Binary Search Tree, Binary Tree
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1) to O(n)
+ * Runtime: 0 ms
+ * Memory: 42.7 MB
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow =slow.next;
+            fast = fast.next.next;
+        }
+        ListNode cur= slow.next;
+        slow.next = null;
+        ListNode pre = null;
+        while (cur != null) {
+            ListNode t= cur.next;
+            cur.next = pre;
+            pre= cur;
+            cur= t;
+        }
+        while (pre != null) {
+            if (pre.val != head.val) {
+                return false;
+            }
+            pre =pre.next;
+            head = head.next;
+        }
+        return true;
+    }
+}
