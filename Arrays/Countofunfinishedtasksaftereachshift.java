0fb7cc616b615d8ad579a3b764b352a4b05bf9cc
Manikant <manikantbindas@gmail.com>
2026-08-02 08:17:18 +0530

    feat: add count-of-unfinished-tasks-after-each-shift (#0)

---
+++ Arrays/Countofunfinishedtasksaftereachshift.java
@@ -0,0 +1,32 @@
+/*
+ * LeetCode Problem 0: count-of-unfinished-tasks-after-each-shift
+ * Problem Number: 0
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/count-of-unfinished-tasks-after-each-shift/
+ *
+ *
+ *
+ * Topics: General
+ * Time Complexity: See solution
+ * Space Complexity: O(1) to O(n)
+ * Runtime: 0 ms
+ * Memory: 43 MB
+ */
+
+class Solution {
+    public int countRatioSubarrays(int[] nums, int a,int b) {
+        int n =nums.length;
+        int ans= 0;
+        for (int i = 0;i< n; i++) {
+            int ev = 0,od =0;
+            for (int j = i; j < n; j++) {
+                if (nums[j]% 2== 0) ev++;
+                else od++;
+                if (od>0 && ev*b <=a* od){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
