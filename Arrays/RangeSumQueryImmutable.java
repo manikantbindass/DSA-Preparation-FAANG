ba1e97651288f294aeaba0bdd020b65ab87b728c
Manikant <manikantbindas@gmail.com>
2026-08-04 17:17:52 +0530

    feat: add Range Sum Query - Immutable (#303)

---
+++ Arrays/RangeSumQueryImmutable.java
@@ -0,0 +1,76 @@
+/*
+ * LeetCode Problem 303: Range Sum Query - Immutable
+ * Problem Number: 303
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/range-sum-query-immutable/
+ *
+ * Given an integer array nums, handle multiple queries of the following type:
+ *
+ * 	Calculate the sum of the elements of nums between indices left and right
+ * inclusive where left <= right.
+ *
+ * Implement the NumArray class:
+ *
+ * 	NumArray(int[] nums) Initializes the object with the integer array nums.
+ * 	int sumRange(int left, int right) Returns the sum of the elements of nums
+ * between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ...
+ * + nums[right]).
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * Output
+ * [null, 1, -1, -3]
+ *
+ * Explanation
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+ * numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+ * numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 	1 <= nums.length <= 104
+ * 	-105 <= nums[i] <= 105
+ * 	0 <= left <= right < nums.length
+ * 	At most 104 calls will be made to sumRange.
+ *
+ * Example 1:
+ * Input: ["NumArray","sumRange","sumRange","sumRange"]
+ * Output: [[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]
+ *
+ * Constraints:
+ * - 1 <= nums.length <= 104
+ * - -105 <= nums[i] <= 105
+ * - 0 <= left <= right < nums.length
+ * - At most 104 calls will be made to sumRange.
+ *
+ * Topics: Array, Design, Prefix Sum
+ * Time Complexity: See solution
+ * Space Complexity: O(1) to O(n)
+ */
+
+class NumArray {
+    private int[] s;
+    public NumArray(int[] nums) {
+        int n =nums.length;
+        s =new int[n+ 1];
+        for (int i=0;i<n;++i) {
+            s[i+1]=s[i]+nums[i];
+        }
+    }
+    public int sumRange(int left, int right) {
+        return s[right+1]-s[left];
+    }
+}
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
