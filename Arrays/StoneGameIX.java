d05eb3a20d14f5d13eab702f9325c128e5ada8e1
Manikant <manikantbindas@gmail.com>
2026-08-17 01:26:14 +0530

    feat: add Stone Game IX (#2156)

---
+++ Arrays/StoneGameIX.java
@@ -0,0 +1,36 @@
+/*
+ * LeetCode Problem 2156: Stone Game IX
+ * Problem Number: 2156
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/stone-game-ix/
+ *
+ *
+ *
+ * Topics: Array, Math, Greedy, Minimax, Counting, Game Theory, Nim Game, Zero-Sum Game
+ * Time Complexity: See solution
+ * Space Complexity: O(1) to O(n)
+ * Runtime: 4
+ * Memory: 114944000
+ */
+
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] c1= new int[3];
+        for (int x: stones) {
+            c1[x%3]++;
+        }
+        int[] c2 = {c1[0], c1[2], c1[1]};
+        return check(c1) ||check(c2);
+    }
+    private boolean check(int[] cnt) {
+        if (--cnt[1] < 0) {
+            return false;
+        }
+        int r = 1 +Math.min(cnt[1],cnt[2])* 2 + cnt[0];
+        if (cnt[1] > cnt[2]) {
+            --cnt[1];
+            ++r;
+        }
+        return r %2==1 && cnt[1]!= cnt[2];
+    }
+}
